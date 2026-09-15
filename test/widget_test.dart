ea2bf670df6f8a99d9dfd0aecb2137379ab72c7f
Cyberboyone <cyberboyone@users.noreply.github.com>
2026-09-15 15:45:47 +0100

    Fix widget_test import and title assertion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:malam_lawan_triumph3/main.dart';
+import 'package:haifan_tafsir/main.dart';
 
 void main() {
   testWidgets('App boots and shows the scholar header', (tester) async {
     await tester.pumpWidget(const IslamicAudioApp());
-
-    expect(
-      find.text('Malam Lawan Triumph 3'),
-      findsOneWidget,
-    );
+    expect(find.text("Shaikh Haifan Tafsir"), findsOneWidget);
   });
 }
